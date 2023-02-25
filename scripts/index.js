f0dac176512300215c7fb253a9c3359a51bc17a2
Kavinkumar-97 <56212561+Kavinkumar-97@users.noreply.github.com>
2023-02-25 09:25:30 +0530

    Implemented List and detail page

---
+++ scripts/index.js
@@ -0,0 +1,149 @@
+
+let favorites = JSON.parse(localStorage.getItem('favorites')) || {};
+let initialMeals = [];
+
+const searchInput = document.getElementById('search-input');
+const mealsList = document.getElementById('meal-list');
+const mealSection = document.getElementById('meal-section');
+const mealHeaderSection = document.getElementById('meal-header-section');
+const searchForm = document.getElementById('search-form');
+
+// Event listeners
+
+searchInput.addEventListener('change', (e) => {
+    const searchText = e.target.value?.trim();
+    performSearch(searchText);
+});
+
+searchForm.addEventListener('submit', (e) => {
+    console.log('Form submission');
+    e.preventDefault();
+    const searchText = searchInput.value?.trim();
+    performSearch(searchText);
+})
+
+searchInput.addEventListener('load', () => {
+    performSearch(searchInput.value || 'a');
+});
+
+performSearch(' ');
+
+async function performSearch(searchText) {
+    try {
+        if (searchText) {
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`);
+            const responseJson = await response.json();
+            const meals = responseJson.meals;
+
+            if (initialMeals.length == 0) {
+                initialMeals = meals;
+            }
+
+            updateMeals(meals, searchText);
+        } else {
+            updateMeals(initialMeals);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function showEmptyPlaceholder(searchText) {
+    mealsList.innerHTML = `No Result Found for ${searchText}`;
+}
+
+function updateMeals(meals, searchText) {
+    if (meals == null || meals.length == 0) {
+        return showEmptyPlaceholder(searchText);
+    }
+
+    mealsList.innerHTML = '';
+
+    for (let meal of meals) {
+        const mealLink = document.createElement('a');
+        mealLink.setAttribute('id', meal.idMeal);
+        mealLink.setAttribute('class', 'meal-link');
+        mealLink.setAttribute('href', `./meal-details.html?id=${meal.idMeal}&title=${meal.strMeal}`);
+
+        const mealContainer = document.createElement('div');
+        mealContainer.setAttribute('class', 'meal-container');
+
+        const mealHeader = document.createElement('div');
+        mealHeader.setAttribute('class', 'meal-header');
+
+        const mealImage = document.createElement('img');
+        mealImage.setAttribute('src', meal.strMealThumb);
+        mealImage.setAttribute('class', 'meal-image');
+
+        mealHeader.append(mealImage);
+
+        const isFavorite = favorites.hasOwnProperty(meal.idMeal);
+
+        const favIcon = isFavorite ? '<i class="fa-sharp fa-solid fa-heart"></i>' : '<i class="fa-regular fa-heart"></i>';
+
+        const favButton = document.createElement('button');
+        favButton.dataset.id = meal.idMeal;
+        favButton.setAttribute('class', `meal-favourite-btn ${isFavorite ? 'fav' : ''}`);
+        favButton.innerHTML = favIcon;
+        favButton.addEventListener('click', (e) => onTapFavourite(e, meal));
+
+        mealHeader.append(favButton);
+
+        const mealContent = document.createElement('div');
+        mealContent.setAttribute('class', 'meal-content');
+
+        const mealTitle = document.createElement('p');
+        mealTitle.setAttribute('class', 'meal-title');
+        mealTitle.innerText = meal.strMeal;
+
+        const mealCategory = document.createElement('p');
+        mealCategory.setAttribute('class', 'meal-category');
+        mealCategory.innerText = meal.strCategory?.trim() || '';
+
+        const mealArea = document.createElement('p');
+        mealArea.setAttribute('class', 'meal-area');
+        mealArea.innerText = meal.strArea?.trim() || '';
+
+        mealContent.append(mealTitle, mealCategory, mealArea);
+        mealContainer.append(mealHeader, mealContent);
+        mealLink.appendChild(mealContainer);
+
+        mealsList.appendChild(mealLink);
+    }
+}
+
+function onTapFavourite(e, meal) {
+    const isFavorite = favorites.hasOwnProperty(meal.idMeal);
+
+    isFavorite ? removeFavorite(e, meal) : addFavorite(e, meal);
+}
+
+function addFavorite(e, meal) {
+    e.preventDefault();
+
+    const favButton = document.querySelector(`.meal-favourite-btn[data-id="${meal.idMeal}"]`);
+    const favIcon = '<i class="fa-sharp fa-solid fa-heart"></i>';
+
+    favButton.classList.add('fav');
+    favButton.innerHTML = favIcon;
+
+    favorites[meal.idMeal] = meal;
+
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+}
+
+function removeFavorite(e, meal) {
+    e.preventDefault();
+
+    console.log(e.target);
+
+    const favButton = document.querySelector(`.meal-favourite-btn[data-id="${meal.idMeal}"]`);
+    const favIcon = '<i class="fa-regular fa-heart"></i>';
+
+    favButton.classList.remove('fav');
+    favButton.innerHTML = favIcon;
+
+    delete favorites[meal.idMeal];
+
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+}
---
+++ scripts/meal-details.js
@@ -0,0 +1,124 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+const title = params.get("title");
+
+const mealDetailSection = document.getElementById('meal-details-section');
+
+let mealDetail = null;
+
+fetchDetail(id);
+
+async function fetchDetail(id) {
+    try {
+        console.log(id);
+        if (id) {
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+            const responseJson = await response.json();
+            const meal = responseJson.meals[0];
+            console.log(meal);
+
+            mealDetail = meal;
+            updateMealDetail(mealDetail);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function updateMealDetail(mealDetail) {
+
+    console.log()
+
+    const mealImage = document.createElement('img');
+    mealImage.setAttribute('id', 'meal-image');
+    mealImage.setAttribute('src', mealDetail.strMealThumb);
+
+    const mealContent = document.createElement('div');
+    mealContent.setAttribute('id', 'meal-content');
+
+    const mealTitle = document.createElement('h1');
+    mealTitle.setAttribute('id', 'meal-title');
+    mealTitle.innerText = mealDetail.strMeal;
+
+    const mealCategory = document.createElement('p');
+    mealCategory.setAttribute('id', 'meal-category');
+    mealCategory.innerText = mealDetail.strArea + ' - ' + mealDetail.strCategory;
+
+    const mealInstructionTitle = document.createElement('h4');
+    mealInstructionTitle.setAttribute('id', 'meal-instruction-title');
+    mealInstructionTitle.setAttribute('class', 'heading');
+
+    const mealInstructionList = document.createElement('div');
+    mealInstructionList.setAttribute('id', 'meal-instruction-list');
+    mealInstructionTitle.innerText = 'Instructions';
+
+    const instructions = mealDetail.strInstructions.split('\r\n').filter(ins => ins.length > 0);
+
+    instructions.forEach((instruction, index) => {
+        const instructionListItem = document.createElement('div');
+        instructionListItem.setAttribute('class', 'meal-instruction');
+
+        const instructionStep = document.createElement('p');
+        instructionStep.setAttribute('class', 'instruction-step');
+        instructionStep.innerText = `STEP ${index + 1}:`;
+
+        const instructionContent = document.createElement('p');
+        instructionContent.setAttribute('class', 'instruction-content');
+
+        const isImportant = instruction[0] == '*';
+        if (isImportant) {
+            instructionContent.setAttribute('class', 'imp');
+        }
+
+        instructionContent.innerText = `${instruction}`;
+
+        instructionListItem.append(instructionStep, instructionContent);
+        mealInstructionList.appendChild(instructionListItem);
+    });
+
+    const mealIngredientTitle = document.createElement('h4');
+    mealIngredientTitle.setAttribute('id', 'meal-ingredient-title');
+    mealIngredientTitle.setAttribute('class', 'heading');
+    mealIngredientTitle.innerText = 'Ingredients';
+
+    const mealIngredient = document.createElement('table');
+    mealIngredient.setAttribute('class', 'ingredients-table');
+
+    let count = 1;
+    let ingredient = mealDetail[`strIngredient${count}`];
+    let measurment = mealDetail[`strMeasure${count}`];
+    while (ingredient?.length > 0 && measurment?.length > 0) {
+
+        const row = document.createElement('tr');
+        row.setAttribute('class', 'ingredient-row');
+
+        const ingredientNode = document.createElement('td');
+        ingredientNode.setAttribute('class', 'ingredient');
+        ingredientNode.innerText = ingredient;
+
+        const measurementNode = document.createElement('td');
+        measurementNode.setAttribute('class', 'measurement');
+        measurementNode.innerText = measurment;
+
+        row.append(ingredientNode, measurementNode);
+        mealIngredient.append(row);
+
+        count += 1;
+        ingredient = mealDetail[`strIngredient${count}`];
+        measurment = mealDetail[`strMeasure${count}`];
+    }
+
+    const mealSource = document.createElement('h4');
+    mealSource.setAttribute('class', 'heading');
+    mealSource.innerText = 'Source';
+
+    const sourceLink = document.createElement('a');
+    sourceLink.setAttribute('href', mealDetail.strYoutube);
+    sourceLink.innerText = mealDetail.strYoutube;
+
+    mealDetailSection.innerHTML = '';
+
+    mealContent.append(mealTitle, mealCategory, mealInstructionTitle, mealInstructionList, mealIngredientTitle, mealIngredient, mealSource, sourceLink);
+
+    mealDetailSection.append(mealImage, mealContent);
+}
